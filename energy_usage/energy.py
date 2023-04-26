bb83b26be111341a0d2eb493f42111876a8de95a
Gertjan van der Meulen <gertjanmeul@kpnmail.nl>
2023-04-26 22:22:32 +0200

    add  redis docker

--- energy_usage/energy.py
+++ energy_usage/energy.py
@@ -7,7 +7,7 @@ class EnergyData:
 
     def __init__(self):
         """Constructor  energy data"""
-        pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+        pool = redis.ConnectionPool(host='localhost', port=16379, db=0)
         r = redis.Redis(connection_pool=pool)
         # r.set('location', 'Tolakkerweg136E')
         self._location = r.get('location')
