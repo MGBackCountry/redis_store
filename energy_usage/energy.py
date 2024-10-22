2fbd5e782321f474503d481b682ca0b27ccb6118
Gertjan van der Meulen <gertjanmeul@kpnmail.nl>
2024-10-22 21:41:40 +0200

    Init config (#2)
    * initial draft
    
    * add  redis docker

---
+++ energy_usage/energy.py
@@ -0,0 +1,19 @@
+"""Data storage classes for energy usage"""
+import redis
+
+
+class EnergyData:
+    """Local Energy Data"""
+
+    def __init__(self):
+        """Constructor  energy data"""
+        pool = redis.ConnectionPool(host='localhost', port=16379, db=0)
+        r = redis.Redis(connection_pool=pool)
+        # r.set('location', 'Tolakkerweg136E')
+        self._location = r.get('location')
+
+
+    @property
+    def location(self):
+        """Getter location energy data"""
+        return self._location
