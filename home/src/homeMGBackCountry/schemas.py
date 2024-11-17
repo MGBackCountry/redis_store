5699663512e35336b6bbb83f5adad4c660dec2ac
Gertjan van der Meulen <gertjanmeul@kpnmail.nl>
2024-11-17 21:00:53 +0100

    Move date check to post_load schema (#5)
    Co-authored-by: Gertjan van der Meulen <gertjan@hollands-groen.nl>

--- home/src/homeMGBackCountry/schemas.py
+++ home/src/homeMGBackCountry/schemas.py
@@ -1,6 +1,8 @@
 """Validation schemas"""
-from marshmallow import Schema, fields
-from datetime import date
+from marshmallow import Schema, fields, post_load
+from flask_smorest import abort
+from datetime import date, datetime
+from dateutil.parser import parse, ParserError
 
 
 class FlowRateSchema(Schema):
@@ -15,12 +17,21 @@ class ElectricitySchema(Schema):
     # noinspection PyTypeChecker
     supply = fields.Nested(FlowRateSchema,required=True)
 
+    @post_load
+    def format_date(self, in_data, **kwargs):
+        """Validate and format date 'YYYY-MM-DD'"""
+        try:
+            in_data["date"] = datetime.strftime(parse(in_data["date"]), "%Y-%m-%d")
+            return in_data
+        except ParserError as error:
+            abort(400, message=str(error))
+
 
 class PatchElectricitySchema(Schema):
     # noinspection PyTypeChecker
-    consume = fields.Nested(FlowRateSchema)
+    consume = fields.Nested(FlowRateSchema,load_only=True)
     # noinspection PyTypeChecker
-    supply = fields.Nested(FlowRateSchema)
+    supply = fields.Nested(FlowRateSchema,load_only=True)
 
 
 class QueryParamsSchema(Schema):
@@ -28,5 +39,5 @@ class QueryParamsSchema(Schema):
     end_date = fields.Date(load_default=date(2100,1,1),load_only=True)
 
 
-class DeletePatchQueryParamsSchema(Schema):
-    date = fields.Date(required=True)
+class DeleteAndPatchQueryParamsSchema(Schema):
+    date = fields.Date(required=True,load_only=True)
