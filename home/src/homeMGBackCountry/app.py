0328b6c3837d94e28ef3a62f6c19c004393ca1f8
Gertjan van der Meulen <gertjan@hollands-groen.nl>
2024-11-17 17:44:12 +0100

    add home endpoint

--- home/src/homeMGBackCountry/app.py
+++ home/src/homeMGBackCountry/app.py
@@ -2,19 +2,19 @@
 
 from flask import Flask
 from flask_smorest import Api
-from resources.electricity import blp as blueprint_electricity
+from resources.home_api import blp as home_api
 
 app = Flask(__name__)
 app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Electricity REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.1.0"
+app.config["API_TITLE"] = "Store data home REST API"
+app.config["API_VERSION"] = "v2.0.0"
+app.config["OPENAPI_VERSION"] = "3.1.1"
 app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 api = Api(app)
-api.register_blueprint(blueprint_electricity)
+api.register_blueprint(home_api)
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8100)
