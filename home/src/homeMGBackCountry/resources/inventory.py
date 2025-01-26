055f9e6261470dfcc8f1037f0ce194b8c1576002
Gertjan van der Meulen <gertjanmeul@kpnmail.nl>
2025-01-26 19:42:59 +0100

    new line proeperty added

--- home/src/homeMGBackCountry/resources/inventory.py
+++ home/src/homeMGBackCountry/resources/inventory.py
@@ -1,26 +1,51 @@
-class Energy:
+from enum import Enum
+from numbers_parser import Document
 
-    def __init__(self, doc):
-        self.doc = doc
+
+class Headers(Enum):
+    WORKSHEET = "/Users/gertjan/Documents/energie rekening/energie_tolakkerweg.numbers"
+    PRIMARYKEYS = ("date", "consume", "supply")
+    SECONDARYKEYS = ("low", "high")
+
+
+class EnergyProvision:
+
+    def __init__(self, worksheet):
+        self._line = None
+        self.worksheet = worksheet
 
     @property
-    def doc(self):
-        return self._doc
+    def worksheet(self):
+        return self._worksheet
 
-    @doc.setter
-    def doc(self, doc):
-        from numbers_parser import Document
-        self._doc = Document(doc).sheets[0].tables[0]
+    @worksheet.setter
+    def worksheet(self, worksheet):
+        self._worksheet = Document(worksheet).sheets[0].tables[0]
+
+    @property
+    def line(self):
+        return self._line
+
+    @line.setter
+    def line(self, line_number: int):
+        formatted_line = [int(i) if isinstance(i, float) else i
+                          for i in self.worksheet.rows(values_only=True)[line_number]]
+        consume = dict(zip(Headers.SECONDARYKEYS.value, formatted_line[1:3]))
+        supply = dict(zip(Headers.SECONDARYKEYS.value, formatted_line[3:5]))
+        self._line = dict(zip(Headers.PRIMARYKEYS.value,
+                          [f"{formatted_line[0]:%Y-%m-%d}", consume, supply]))
 
 if __name__ == "__main__":
-    fname = "/Users/gertjan/Documents/energie rekening/energie_tolakkerweg.numbers"
-    keys = ["date", "consume", "supply"]
-    subkeys = ["low", "high"]
-    energy = Energy(fname)
-    for row in energy.doc.iter_rows(min_row=1, min_col=0, max_row=energy.doc.num_rows-1,
-                                   max_col=5, values_only=True):
-        formatted_row = [int(i) if isinstance(i,float) else i for i in row]
-        consume = dict(zip(subkeys, formatted_row[1:3]))
-        supply = dict(zip(subkeys, formatted_row[3:5]))
-        energy = dict(zip(keys, [f"{row[0]:%Y-%m-%d}", consume, supply]))
-        print(energy)
+    energy_provision = EnergyProvision(Headers.WORKSHEET.value)
+    energy_provision.line = energy_provision.worksheet.num_rows - 1
+    print(energy_provision.line)
+    '''
+    for row in energy_provision.worksheet.iter_rows(min_row=1, min_col=0,
+                                                    max_row=energy_provision.worksheet.num_rows - 1,
+                                                    max_col=5, values_only=True):
+        formatted_row = [int(i) if isinstance(i, float) else i for i in row]
+        consume = dict(zip(Headers.SECONDARYKEYS.value, formatted_row[1:3]))
+        supply = dict(zip(Headers.SECONDARYKEYS.value, formatted_row[3:5]))
+        energy_provision = dict(zip(Headers.PRIMARYKEYS.value, [f"{row[0]:%Y-%m-%d}", consume, supply]))
+        print(energy_provision)
+    '''
